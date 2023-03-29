dd01bb138baaccfc06b6acf1c7813c0397d8611a
adelinashahaj <adelinarucaj@icloud.com>
2023-03-29 14:14:08 +0200

    first commit

---
+++ src/App.vue
@@ -0,0 +1,22 @@
+<template>
+
+  <MyHeader title="YU-GI-OH Api"/>
+
+</template>
+
+<script>
+import MyHeader from './components/MyHeader.vue';
+export default{
+  components: {
+    MyHeader
+  },
+
+}
+
+</script>
+
+
+<style lang="scss">
+ @use './styles/general.scss'
+
+</style>
